50414e059882d50220db4b79dfab4d13aa502db5
Sigui Thierry Herman Yabre <thierry.yabre@yahoo.fr>
2022-07-21 16:03:51 -0400

    commit tree ed2882c84c376bcee4e44923fb735ab8107a5dc8

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+fetch("https://anapioficeandfire.com/api/books").then(resp => console.log(resp.json)).then(data =>console.log(data));    
 }
 
 function renderBooks(books) {
